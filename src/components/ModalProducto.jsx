a27c482da051009d20137c56a243c2a229000101
Hugo <hugo@boahstudio.com>
2023-10-25 00:43:53 -0300

    product quantity selector

--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import useQuiosco from '../hooks/useQuiosco'
 import { formatearDinero } from '../helpers'
 
 export default function ModalProducto() {
 
     const { producto, handleClickModal  } = useQuiosco()
+    const [cantidad, setCantidad] = useState(1)
+
+
 
   return (
     <div>
@@ -30,6 +34,36 @@ export default function ModalProducto() {
                 <p className='mt-5 font-black text-5xl text-amber-500'>
                     {formatearDinero(producto.precio)}
                 </p>
+
+                <div className='flex gap-4 mt-5'>
+                    <button
+                        type='button'
+                        onClick={() => {
+                            if (cantidad <= 1) return
+                            setCantidad(cantidad - 1)
+                        }}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </button>
+                    <p className='text-3xl'>
+                        {cantidad}
+                    </p>
+                    <button
+                        type='button'
+                        onClick={() => {
+                            if (cantidad >= 5) return
+                            setCantidad(cantidad + 1)
+                        }}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </button>
+                </div>
+
+
                 <button
                     type='button'
                     className='bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded'
